bcf986266094e3813f561249d109810c0d492449
CrazyBoy49z <yura.finiv@gmail.com>
2023-07-16 21:45:13 +0300

    Add GenerateRoute service for enhanced routing
    This commit introduces a new file 'GenerateRoute.php' in 'src/Services'. It contains the class 'GenerateRoute' which dynamically generates and manages routes for the application. This service offers more flexibility in handling different types of routes and their parameters, and also improves the maintainability of the code by encapsulating the route generation logic in a dedicated service.

---
+++ src/Services/GenerateRoute.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Step2dev\LazyAdmin\Services;
+
+use Illuminate\Support\Facades\Route;
+
+class GenerateRoute
+{
+    final public function make(): static
+    {
+        return new static();
+    }
+
+    /** @noinspection MethodShouldBeFinalInspection */
+    public function generateRoutes(): array
+    {
+        $defaultRoutes = $this->getDefaultRoutes();
+
+        $routes = [];
+
+        $routeName = $this->getRouteName();
+
+        foreach ($defaultRoutes as $key => $route) {
+            $routes[$key] = null;
+
+            if ($route['name'] && str($routeName)->contains($route['showOn'])) {
+                if (in_array($key, ['index', 'create'])) {
+                    $routes[$key] = [
+                        'url'    => route($route['name']),
+                        'target' => $route['target'] ?? '_self',
+                    ];
+                } else {
+                    $routes[$key] = [
+                        'url'    => $route['url'] ?? route($route['name'], $this->getRouteParameters()),
+                        'target' => $route['target'] ?? '_self',
+                    ];
+                }
+            }
+        }
+
+        return $routes;
+    }
+
+    public function getDefaultRoutes(): array
+    {
+        return [
+            'index'   => [
+                'name'   => $this->getRouteIndexName(),
+                'showOn' => [],
+            ],
+            'create'  => [
+                'name'   => $this->getRouteCreateName(),
+                'showOn' => ['index', 'edit'],
+            ],
+            'show'    => [
+                'name'   => $this->getRouteShowName(),
+                'showOn' => ['edit'],
+            ],
+            'edit'    => [
+                'name'   => $this->getRouteEditName(),
+                'showOn' => ['show'],
+            ],
+            'destroy' => [
+                'name'   => $this->getRouteDestroyName(),
+                'showOn' => ['show', 'edit'],
+            ],
+        ];
+    }
+
+    public function getRouteIndexName(): ?string
+    {
+        return $this->generateRouteNameByKey();
+    }
+
+    public function generateRouteNameByKey(string $key = 'index'): ?string
+    {
+        $routeName = $this->getRoutePrefixNameWithDot().$key;
+
+        if (Route::has($routeName)) {
+            return $routeName;
+        }
+
+        return null;
+    }
+
+    public function getRoutePrefixNameWithDot(): ?string
+    {
+        return $this->getRoutePrefixName().'.';
+    }
+
+    public function getRoutePrefixName(): ?string
+    {
+        return str($this->getRouteName())->beforeLast('.');
+    }
+
+    public function getRouteName(): ?string
+    {
+        return Route::currentRouteName();
+    }
+
+    public function getRouteCreateName(): ?string
+    {
+        return $this->generateRouteNameByKey('create');
+    }
+
+    public function getRouteShowName(): ?string
+    {
+        return $this->generateRouteNameByKey('show');
+    }
+
+    public function getRouteEditName(): ?string
+    {
+        return $this->generateRouteNameByKey('edit');
+    }
+
+    public function getRouteDestroyName(): ?string
+    {
+        return $this->generateRouteNameByKey('destroy');
+    }
+
+    public function getRouteParameters(): ?array
+    {
+        return request()?->route()?->parameters();
+    }
+}
