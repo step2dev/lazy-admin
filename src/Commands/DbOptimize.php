6ed7b18d20f815694ba8da82aa50de6824e917bc
CrazyBoy49z <yura.finiv@gmail.com>
2024-05-10 18:13:35 +0300

    Add delete-workflows action and refactor DbOptimize command
    Added a new GitHub action for deleting old workflow runs based on user-defined conditions. In the codebase, the DbOptimize command was refactored for better clarity and efficiency, moving some functions around and improving the existsTables check. The code now uses the Query Builder's whereIn method instead of raw SQL.

--- src/Commands/DbOptimize.php
+++ src/Commands/DbOptimize.php
@@ -5,6 +5,7 @@ namespace Step2dev\LazyAdmin\Commands;
 use Illuminate\Console\Command as BaseCommand;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection;
+use RuntimeException;
 
 final class DbOptimize extends BaseCommand
 {
@@ -45,9 +46,33 @@ final class DbOptimize extends BaseCommand
     {
         $this->info('Starting Optimization.');
         $this->getTables()
-            ->tap(fn ($collection) => $this->progress = $this->output->createProgressBar($collection->count()))
-            ->each(fn ($table) => $this->optimize($table));
+            ->tap(fn($collection) => $this->progress = $this->output->createProgressBar($collection->count()))
+            ->each(fn($table) => $this->optimize($table));
         $this->info(PHP_EOL.'Optimization Completed');
+    }
+
+    /**
+     * Get all the tables that need to the optimized
+     */
+    private function getTables(): Collection
+    {
+        $tableList = collect($this->option('table'));
+        if ($tableList->isEmpty()) {
+            return $this->db
+                ->newQuery()
+                ->selectRaw('TABLE_NAME')
+                ->fromRaw('INFORMATION_SCHEMA.TABLES')
+                ->whereRaw("TABLE_SCHEMA = '{$this->getDatabase()}'")
+                ->whereRaw("TABLE_TYPE = 'BASE TABLE'")
+                ->whereRaw("ENGINE = 'InnoDB'")
+                ->whereRaw("TABLE_NAME NOT LIKE 'pma%'")
+                ->pluck('TABLE_NAME');
+        }
+        // Check if the table exists
+        if ($this->existsTables($tableList)) {
+            return $tableList;
+        }
+        throw new RuntimeException("One or more tables provided doesn't exists.");
     }
 
     /**
@@ -60,15 +85,13 @@ final class DbOptimize extends BaseCommand
         if ($database === 'default') {
             $database = config('database.default');
 
-            return config("database.connections.{$database}.database");
+            return config('database.connections.'.$database.'.database');
         }
-
         // Check if the database exists
         if (is_string($database) && $this->existsDatabase($database)) {
             return $database;
         }
-
-        throw new \RuntimeException("This database {$database} doesn't exists.");
+        throw new RuntimeException("This database {$database} doesn't exists.");
     }
 
     /**
@@ -85,45 +108,16 @@ final class DbOptimize extends BaseCommand
     }
 
     /**
-     * Get all the tables that need to the optimized
-     */
-    private function getTables(): Collection
-    {
-        $tableList = collect($this->option('table'));
-
-        if ($tableList->isEmpty()) {
-            return $this->db
-                ->newQuery()
-                ->selectRaw('TABLE_NAME')
-                ->fromRaw('INFORMATION_SCHEMA.TABLES')
-                ->whereRaw("TABLE_SCHEMA = '{$this->getDatabase()}'")
-                ->whereRaw("TABLE_TYPE = 'BASE TABLE'")
-                ->whereRaw("ENGINE = 'InnoDB'")
-                ->whereRaw("TABLE_NAME NOT LIKE 'pma%'")
-                ->pluck('TABLE_NAME');
-        }
-
-        // Check if the table exists
-        if ($this->existsTables($tableList)) {
-            return $tableList;
-        }
-
-        throw new \RuntimeException("One or more tables provided doesn't exists.");
-    }
-
-    /**
      * Check if the table exists
      */
     private function existsTables(Collection $tables): bool
     {
-        $tableNames = $tables->implode("','");
-
         return $this->db
-            ->newQuery()
-            ->fromRaw('INFORMATION_SCHEMA.TABLES')
-            ->whereRaw('TABLE_SCHEMA = \'?\'', [$this->getDatabase()])
-            ->whereRaw('TABLE_NAME IN (\'?\')', [$tableNames])
-            ->count() === $tables->count();
+                ->newQuery()
+                ->from('INFORMATION_SCHEMA.TABLES')
+                ->where('TABLE_SCHEMA', $this->getDatabase())
+                ->whereIn('TABLE_NAME', $tables)
+                ->count() === $tables->count();
     }
 
     /**
@@ -131,7 +125,7 @@ final class DbOptimize extends BaseCommand
      */
     protected function optimize(string $table): void
     {
-        $result = $this->db->getConnection()->select("OPTIMIZE TABLE `{$table}`", true);
+        $result = $this->db->getConnection()->select("OPTIMIZE TABLE `{$table}`");
 
         if (collect($result)->pluck('Msg_text')->contains('OK')) {
             $this->progress->advance();
