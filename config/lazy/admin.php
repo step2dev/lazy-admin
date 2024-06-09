1efc773449b95ce2251b512e918c68fd55191f92
CrazyBoy49z <yura.finiv@gmail.com>
2024-06-09 19:45:49 +0300

    Remove theme-switcher and socialite client details
    The theme-switcher options and theme list were removed from the admin configuration file. Similarly, details about different socialite clients such as client id, secret, and redirect were removed from the socialite configuration file. These changes suggest a simplification or a shift to a new approach for handling themes and socialite clients in the application.

--- config/lazy/admin.php
+++ config/lazy/admin.php
@@ -4,55 +4,6 @@
 use Step2dev\LazyAdmin\Middleware\LazyAdminMiddleware;
 
 return [
-    /*
-     * This type of toggle will be used for the theme switcher.
-     * Available options: multiple, single, toggle
-     */
-    'theme-toggle' => 'multiple',
-    /*
-     * This is the list of themes that will be available for the theme switcher.
-     * You can add your own themes here.
-     */
-    'themes' => [
-        'light',
-        'dark',
-        'acid',
-        'aqua',
-        'autumn',
-        'black',
-        'bumblebee',
-        'business',
-        'cmyk',
-        'coffee',
-        'corporate',
-        'cupcake',
-        'cyberpunk',
-        'dracula',
-        'emerald',
-        'fantasy',
-        'forest',
-        'garden',
-        'halloween',
-        'lemonade',
-        'lofi',
-        'luxury',
-        'night',
-        'pastel',
-        'retro',
-        'synthwave',
-        'valentine',
-        'winter',
-        'wireframe',
-    ],
-
-    /*
-     * This is list to toggle themes.
-     */
-    'toggle_themes' => [
-        'light',
-        'dark',
-    ],
-
     'route' => [
         'name' => env('LAZY_ROUTE_NAME', 'admin.'),
         'prefix' => env('LAZY_ROUTE_PREFIX', 'admin'),
--- config/lazy/socialite.php
+++ config/lazy/socialite.php
@@ -5,39 +5,21 @@ return [
     'services' => [
         'facebook' => [
             'enable' => env('FACEBOOK_CLIENT_ID', false),
-            'client_id' => env('FACEBOOK_CLIENT_ID'),
-            'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-            'redirect' => env('FACEBOOK_REDIRECT'),
         ],
         'google' => [
             'enable' => env('GOOGLE_CLIENT_ID', false),
-            'client_id' => env('GOOGLE_CLIENT_ID'),
-            'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-            'redirect' => env('GOOGLE_REDIRECT'),
         ],
         'twitter' => [
             'enable' => env('TWITTER_CLIENT_ID', false),
-            'client_id' => env('TWITTER_CLIENT_ID'),
-            'client_secret' => env('TWITTER_CLIENT_SECRET'),
-            'redirect' => env('TWITTER_REDIRECT'),
         ],
         'github' => [
             'enable' => env('GITHUB_CLIENT_ID', false),
-            'client_id' => env('GITHUB_CLIENT_ID'),
-            'client_secret' => env('GITHUB_CLIENT_SECRET'),
-            'redirect' => env('GITHUB_REDIRECT'),
         ],
         'linkedin' => [
             'enable' => env('LINKEDIN_CLIENT_ID', false),
-            'client_id' => env('LINKEDIN_CLIENT_ID'),
-            'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
-            'redirect' => env('LINKEDIN_REDIRECT'),
         ],
         'apple' => [
             'enable' => env('APPLE_CLIENT_ID', false),
-            'client_id' => env('APPLE_CLIENT_ID'),
-            'client_secret' => env('APPLE_CLIENT_SECRET'),
-            'redirect' => env('APPLE_REDIRECT'),
         ],
     ],
 ];
