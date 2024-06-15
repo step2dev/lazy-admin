0470e4ee268542cc66b47d2358b09fc09d696e17
CrazyBoy49z <yura.finiv@gmail.com>
2024-06-15 18:12:41 +0300

    Add properties to Setting model
    The commit introduces several properties to the Setting model. These new properties include "group", "key", "value", "type", "is_protected", "is_encrypted", "deletable", and "settingKey". This change improves the documentation and readability of the code.

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -5,6 +5,16 @@ namespace Step2dev\LazyAdmin\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string|null $group
+ * @property string $key
+ * @property string $value
+ * @property string $type
+ * @property bool $is_protected
+ * @property bool $is_encrypted
+ * @property bool $deletable
+ * @property-read string $settingKey
+ */
 class Setting extends Model
 {
     use HasFactory;
