719c46aae8de08461f63de7818a149e7954a8866
CrazyBoy49z <CrazyBoy49z@users.noreply.github.com>
2024-06-15 17:25:42 +0000

    Fix styling

--- tests/src/Services/SettingServiceTest.php
+++ tests/src/Services/SettingServiceTest.php
@@ -2,22 +2,22 @@
 
 namespace Step2dev\LazyAdmin\Tests\Services;
 
-use Step2dev\LazyAdmin\Services\SettingService;
 use Step2dev\LazyAdmin\Models\Setting;
+use Step2dev\LazyAdmin\Services\SettingService;
 
 it('returns the correct setting value', function () {
     // Arrange
-//    Setting::factory()->create([
-//        'group' => 'default', // 'default' or 'test_group'
-//        'key'   => 'test_key',
-//        'value' => 'test_value',
-//    ]);
+    //    Setting::factory()->create([
+    //        'group' => 'default', // 'default' or 'test_group'
+    //        'key'   => 'test_key',
+    //        'value' => 'test_value',
+    //    ]);
 
-//    $service = new SettingService();
+    //    $service = new SettingService();
 
     // Act
-//    $value = $service->get('default.test_key');
+    //    $value = $service->get('default.test_key');
 
     // Assert
-//    expect($value)->toBe('test_value');
+    //    expect($value)->toBe('test_value');
 });
