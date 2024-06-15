e8d45d0a3d6c6bc455de3051ce584991ce5aecb6
CrazyBoy49z <yura.finiv@gmail.com>
2024-06-15 20:25:19 +0300

    Update SettingFactory and add SettingServiceTest
    The SettingFactory has been updated to include a variety of new data types for generating fake data. In addition, a test file for SettingService has been created, although it's currently commented-out and needs further development.

---
+++ tests/src/Services/SettingServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Step2dev\LazyAdmin\Tests\Services;
+
+use Step2dev\LazyAdmin\Services\SettingService;
+use Step2dev\LazyAdmin\Models\Setting;
+
+it('returns the correct setting value', function () {
+    // Arrange
+//    Setting::factory()->create([
+//        'group' => 'default', // 'default' or 'test_group'
+//        'key'   => 'test_key',
+//        'value' => 'test_value',
+//    ]);
+
+//    $service = new SettingService();
+
+    // Act
+//    $value = $service->get('default.test_key');
+
+    // Assert
+//    expect($value)->toBe('test_value');
+});
