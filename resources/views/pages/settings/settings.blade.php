a8357bc765dd20f12b41639c2677b805452ebc7f
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-16 12:20:08 +0300

    Clean up lazy admin settings view

--- resources/views/pages/settings/settings.blade.php
+++ resources/views/pages/settings/settings.blade.php
@@ -1,10 +1,7 @@
 <div>
     <x-lazy-form-input label="Name" wire:model="settings.name"/>
-    <x-lazy-form-textarea label="Email" wire:model="settings.description"/>
+    <x-lazy-form-textarea label="Description" wire:model="settings.description"/>
+    <x-lazy-form-image label="Logo" wire:model="logo" :src="setting('admin.logo')"/>
+
     <x-lazy-btn primary label="Save" wire:click="save"/>
-    <x-lazy-form-image label="Logo" required wire:model="settings.logo" :src="setting('site.logo')"/>
-    @dump($settings)
-    @foreach(lazyLocalization()->getSupportedLocales() as $locale)
-        @dump($locale)
-    @endforeach
 </div>
