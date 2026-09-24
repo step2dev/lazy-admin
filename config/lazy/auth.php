e08b74ba9d7af7f2d041ad3850881b5cf05566ae
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-24 17:23:57 +0300

    refactor: decouple auth config from app user model

--- config/lazy/auth.php
+++ config/lazy/auth.php
@@ -1,17 +1,18 @@
 <?php
 
-use App\Models\User;
-
 return [
+    'guard' => env('LAZY_AUTH_GUARD', 'web'),
+
+    'provider' => env('LAZY_AUTH_PROVIDER'),
+
     'login_fields' => [
         'email',
-        //        'username',
-        //        'phone'
     ],
-    'providers' => [
-        'users' => [
-            'model' => User::class, // @phpstan-ignore-line
-        ],
+
+    'login' => [
+        'enabled' => env('LAZY_AUTH_LOGIN_ENABLED', false),
+        'redirect_route' => env('LAZY_AUTH_REDIRECT_ROUTE', 'admin.dashboard'),
     ],
+
     'password_timeout' => 10800,
 ];
