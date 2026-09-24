e98f1f845f3a0e6fd861e550d4b0c59341784754
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-24 20:33:50 +0300

    fix: normalize legacy menu icon views and raw SVG icons

--- resources/views/menu-label.blade.php
+++ resources/views/menu-label.blade.php
@@ -1,8 +1,25 @@
+@php
+    $iconView = $item['icon_view'] ?? null;
+    $icon = $item['icon'] ?? null;
+
+    if (is_string($iconView) && ! str_contains($iconView, '::')) {
+        $lazyIconView = 'lazy::'.$iconView;
+
+        if (view()->exists($lazyIconView)) {
+            $iconView = $lazyIconView;
+        }
+    }
+
+    $rawSvgIcon = is_string($icon) && str_starts_with(ltrim($icon), '<svg');
+@endphp
+
 <span class="relative inline-flex h-6 w-6 shrink-0 items-center justify-center" aria-hidden="true">
-    @if(! empty($item['icon_view']))
-        <span class="[&>svg]:h-6 [&>svg]:w-6">@include($item['icon_view'])</span>
-    @elseif(! empty($item['icon']))
-        <i class="{{ $item['icon'] }}"></i>
+    @if(is_string($iconView) && view()->exists($iconView))
+        <span class="[&>svg]:h-6 [&>svg]:w-6">@include($iconView)</span>
+    @elseif($rawSvgIcon)
+        <span class="[&>svg]:h-6 [&>svg]:w-6">{!! $icon !!}</span>
+    @elseif(filled($icon))
+        <i class="{{ $icon }}"></i>
     @endif
 
     @if(isset($item['badge']))
