e74bfd7ac8dfc10bacddd2286d53b0a6a410152a
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-24 19:54:27 +0300

    feat: improve compact admin sidebar

--- resources/views/menu-generator.blade.php
+++ resources/views/menu-generator.blade.php
@@ -1,6 +1,6 @@
 <ul
-    class="menu bg-base-200 rounded-box transition-all duration-200"
-    :class="sidebarCompact ? 'w-20 menu-compact' : 'w-56'"
+    class="menu bg-base-200 rounded-box w-full overflow-visible p-2 transition-all duration-200"
+    :class="sidebarCompact ? 'menu-compact' : ''"
 >
     @foreach($menuItems as $item)
         @include('lazy::menu-item', ['item' => $item])
