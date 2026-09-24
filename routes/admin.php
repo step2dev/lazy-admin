8763d07b044a35d15f5d36a2a59e450a2059f333
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-25 02:27:54 +0300

    Use a unified access management route

--- routes/admin.php
+++ routes/admin.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Step2dev\LazyAdmin\Controllers\AccessController;
 use Step2dev\LazyAdmin\Controllers\PermissionController;
 use Step2dev\LazyAdmin\Controllers\RoleController;
 use Step2dev\LazyAdmin\Controllers\SettingController;
 use Step2dev\LazyAdmin\Controllers\UserController;
 
 Route::resource('user', UserController::class);
-Route::resource('role', RoleController::class)->except(['show']);
-Route::resource('permission', PermissionController::class)->except(['show']);
+Route::get('access', AccessController::class)->name('access.index');
+
+Route::resource('role', RoleController::class)->only(['store', 'update', 'destroy']);
+Route::resource('permission', PermissionController::class)->only(['store', 'update', 'destroy']);
 
 Route::singletons([
     'setting' => SettingController::class,
