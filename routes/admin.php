23d324fec7daad47ed738a6fe0f4db97fe46ab25
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-25 02:53:41 +0300

    Keep host admin route file free of core access routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Step2dev\LazyAdmin\Controllers\AccessController;
-use Step2dev\LazyAdmin\Controllers\PermissionController;
-use Step2dev\LazyAdmin\Controllers\RoleController;
 use Step2dev\LazyAdmin\Controllers\SettingController;
 use Step2dev\LazyAdmin\Controllers\UserController;
 
 Route::resource('user', UserController::class);
-Route::get('access', AccessController::class)->name('access.index');
-
-Route::resource('role', RoleController::class)->only(['store', 'update', 'destroy']);
-Route::resource('permission', PermissionController::class)->only(['store', 'update', 'destroy']);
-
 Route::singletons([
     'setting' => SettingController::class,
 ], [
