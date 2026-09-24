90ae242729c8ef46716967166fa8b15110ce667e
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-25 01:43:38 +0300

    Register role and permission admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Step2dev\LazyAdmin\Controllers\PermissionController;
+use Step2dev\LazyAdmin\Controllers\RoleController;
 use Step2dev\LazyAdmin\Controllers\SettingController;
 use Step2dev\LazyAdmin\Controllers\UserController;
 
 Route::resource('user', UserController::class);
+Route::resource('role', RoleController::class)->except(['show']);
+Route::resource('permission', PermissionController::class)->except(['show']);
 
 Route::singletons([
     'setting' => SettingController::class,
