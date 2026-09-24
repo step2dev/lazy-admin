7e3e1a76e46f7cd8afc7730b4c8b02b9412395ab
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-24 19:04:10 +0300

    feat: enable user show route

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Step2dev\LazyAdmin\Controllers\SettingController;
 use Step2dev\LazyAdmin\Controllers\UserController;
 
-Route::resource('user', UserController::class)->except('show');
+Route::resource('user', UserController::class);
 
 Route::singletons([
     'setting' => SettingController::class,
