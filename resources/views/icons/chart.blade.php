00978b1830bd1709eef9ccb1a832f93eb3185473
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-24 19:44:50 +0300

    feat: add reusable admin menu icons

---
+++ resources/views/icons/chart.blade.php
@@ -0,0 +1,1 @@
+<svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M4 19V9m5 10V5m5 14v-7m5 7V3"/></svg>
