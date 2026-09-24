10f830c639c0c7c1ad7a6fe729d7e89049e4950b
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-25 01:48:22 +0300

    Skip permission seeding before migrations exist

--- database/Seeders/PermissionSeeder.php
+++ database/Seeders/PermissionSeeder.php
@@ -3,12 +3,24 @@
 namespace Step2dev\LazyAdmin\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 use Step2dev\LazyAdmin\Authorization\AuthorizationManager;
 
 class PermissionSeeder extends Seeder
 {
     public function run(): void
     {
+        $tables = (array) config('permission.table_names', []);
+
+        if (
+            empty($tables['roles'])
+            || empty($tables['permissions'])
+            || ! Schema::hasTable($tables['roles'])
+            || ! Schema::hasTable($tables['permissions'])
+        ) {
+            return;
+        }
+
         app(AuthorizationManager::class)->seedDefaults();
     }
 }
