7481d766cfbdc3c3950c762afeacd9989bad1870
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-24 19:43:17 +0300

    feat: support compact lazy-admin menu

--- resources/views/menu-generator.blade.php
+++ resources/views/menu-generator.blade.php
@@ -1,4 +1,7 @@
-<ul class="menu bg-base-200 w-56 rounded-box">
+<ul
+    class="menu bg-base-200 rounded-box transition-all duration-200"
+    :class="sidebarCompact ? 'w-20 menu-compact' : 'w-56'"
+>
     @foreach($menuItems as $item)
         @include('lazy::menu-item', ['item' => $item])
     @endforeach
